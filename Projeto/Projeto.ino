43bd7de82b0524112dd1de7d718a0de127570234
Gabriel Figenio <gabebe03@gmail.com>
2024-11-09 20:17:44 -0300

    Segunda tentativa falha com KERAS

--- Projeto/Projeto.ino
+++ Projeto/Projeto.ino
@@ -1,15 +1,19 @@
 #include <Arduino.h>
+#include "model.h"
+#include <tflm_esp32.h>
+#include <eloquent_tinyml.h>
 #include <driver/i2s.h>
 #include <stdio.h>
 #include <math.h>
 #include "arduinoFFT.h"
 #include "libmfcc.h"
-#include "variables.h"
 
 //==============I2C Macros 
 // you shouldn't need to change these settings
-#define SAMPLE_BUFFER_SIZE 512
+#define SAMPLE_BUFFER_SIZE 8
 #define SAMPLE_RATE 8000
+#define LEN 8192
+#define HALFLEN 4096
 
 // most microphones will probably default to left channel but you may need to tie the L/R pin low
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
@@ -18,31 +22,30 @@
 #define I2S_MIC_LEFT_RIGHT_CLOCK 25
 #define I2S_MIC_SERIAL_DATA 32
 
-#define LEN 4096
 
 //==============Other Macros 
 #define LED 15
 #define BUTTON 26
+#define ARENA_SIZE 5000
 
 //==============Global variables
 int16_t raw_samples[SAMPLE_BUFFER_SIZE];
-//float features[LEN];
-//float features[4096];
+float features[LEN];
+double vReal[HALFLEN];
+double vImag[HALFLEN];
+float entrada[10];
 
 int indx = 0;
 unsigned int coeff;
 bool botao = 0;
 bool antbotao = 0;
+float trust = 0.8;
+int limit = 2000;
+float divisor;
 
-double vReal[LEN];
-double vImag[LEN];
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, HALFLEN, SAMPLE_RATE);
 
-ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, LEN, SAMPLE_RATE);
-
-#define SCL_INDEX 0x00
-#define SCL_TIME 0x01
-#define SCL_FREQUENCY 0x02
-#define SCL_PLOT 0x03
+Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> tf;
 
 
 i2s_config_t i2s_config = 
@@ -53,8 +56,8 @@ i2s_config_t i2s_config =
   .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
   .communication_format = I2S_COMM_FORMAT_I2S,
   .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-  .dma_buf_count = 2,
-  .dma_buf_len = 1024,
+  .dma_buf_count = 4,
+  .dma_buf_len = SAMPLE_BUFFER_SIZE*2,
   .use_apll = false,
   .tx_desc_auto_clear = false,
   .fixed_mclk = 0
@@ -78,6 +81,18 @@ void setup()
   i2s_set_pin(I2S_NUM_0, &i2s_mic_pins);
   i2s_start(I2S_NUM_0);
 
+  delay(500);
+
+  registerNetworkOps(tf);
+
+  while (!tf.begin(model).isOk()) 
+  {
+    Serial.println(tf.exception.toString());
+    delay(1000);
+  }
+
+  delay(500);
+
   pinMode(LED,OUTPUT);
   pinMode(BUTTON,INPUT);
   
@@ -85,12 +100,14 @@ void setup()
 
 void loop()
 {
-  /*
+  
   // Escutar com o microfone=================================================================================
   size_t bytes_read = 0;
   i2s_read(I2S_NUM_0, raw_samples, sizeof(int16_t) * SAMPLE_BUFFER_SIZE, &bytes_read, portMAX_DELAY);
   int samples_read = bytes_read / sizeof(int16_t);
+
   
+        
   // Agrupar várias amostras=================================================================================
   for(int i=0; i<samples_read; i++)
   {
@@ -100,17 +117,35 @@ void loop()
   
   
   // Quando fechar o conjunto de amostras====================================================================
+  
   if(indx >= LEN)
-  {*/
-
-  botao = digitalRead(BUTTON);
-
-  if(botao && !antbotao)
   {
     indx = 0;
+    int tamanho = LEN;
 
-    int tamanho = sizeof(features) / sizeof(features[0]);
+    // Normalizar audio========================================================================================
+    float bigger = 0;
+    for (int i = 0; i < tamanho; i++) 
+    {
+      if (features[i] > bigger) bigger = features[i];
+    }
 
+    if(bigger > limit)
+    {
+      divisor = bigger;
+    }
+    else
+    {
+      divisor = limit;
+    }
+
+    for (int i = 0; i < tamanho; i++) 
+    {
+      features[i] = features[i]/divisor;
+    }
+
+    
+    
     // Variáveis de cálculo
     float soma = 0.0, somaQuadrados = 0.0;
     float maxAbs, maior = features[0], menor = features[0];
@@ -125,14 +160,14 @@ void loop()
         soma += valor;
 
         // Soma absoluta para Shape e Impulse Factor
-        somaAbsoluta += fabs(valor);
+        //somaAbsoluta += fabs(valor);
 
         // Soma dos quadrados para RMS e Power
         somaQuadrados += valor * valor;
         somaQuadradoAbsoluto += fabs(valor) * fabs(valor);
         
         //Soma das raizes
-        somaRaiz += sqrt(fabs(valor));
+        //somaRaiz += sqrt(fabs(valor));
 
         // Maior e menor valor
         if (valor > maior) maior = valor;
@@ -158,34 +193,34 @@ void loop()
     float media = soma / tamanho;
 
     // Calcular variância e desvio padrão
-    float variancia = (somaQuadrados / tamanho) - (media * media);
-    float desvioPadrao = sqrt(variancia);
+    //loat variancia = (somaQuadrados / tamanho) - (media * media);
+    //float desvioPadrao = sqrt(variancia);
 
     // Calcular RMS
     float rms = sqrt(somaQuadrados / tamanho);
 
     // Calcular Power
-    float power = somaQuadrados / tamanho;
+    //float power = somaQuadrados / tamanho;
 
     // Calcular Crest Factor
     float crestFactor = maxAbs / rms;
 
     // Calcular Shape Factor
-    float shapeFactor = rms / (somaAbsoluta / tamanho); 
+    //float shapeFactor = rms / (somaAbsoluta / tamanho); 
 
     // Calcular Impulse Factor
-    float impulseFactor = maxAbs / (somaAbsoluta / tamanho); 
+    //float impulseFactor = maxAbs / (somaAbsoluta / tamanho); 
 
     // Calcular Clearance Factor
-    float clearanceFactor = maxAbs / pow(somaRaiz / tamanho,2);
+    //float clearanceFactor = maxAbs / pow(somaRaiz / tamanho,2);
 
     // Variáveis para Kurtosis e Skewness
     float somaCubos = 0.0, somaQuartaPotencia = 0.0;
 
     // Segundo loop: calcular soma dos cubos e quarta potência para Kurtosis e Skewness
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < HALFLEN; i++) {
         float valor = features[i];
-        somaCubos += pow(valor - media, 3);            // Soma dos cubos para Skewness
+        //somaCubos += pow(valor - media, 3);            // Soma dos cubos para Skewness
         somaQuartaPotencia += pow(valor - media, 4);   // Soma da quarta potência para Kurtosis
     }
 
@@ -193,66 +228,23 @@ void loop()
     float kurtosis = (somaQuartaPotencia / tamanho) / pow(rms, 4) - 3;
 
     // Calcular Skewness
-    float skewness = (somaCubos / tamanho) / pow(rms, 3);
+    //float skewness = (somaCubos / tamanho) / pow(rms, 3);
 
     // Diferença entre maior e menor valor
-    float diferencaMaiorMenor = maior - menor;
+    //float diferencaMaiorMenor = maior - menor;
 
     
-    for (uint16_t i = 0; i < tamanho; i++)
+    for (uint16_t i = 0; i < HALFLEN; i++)
     {
-      vReal[i] = features[i];//Build data with positive and negative values
+      vReal[i] = features[LEN/4+i];//Build data with positive and negative values
       vImag[i] = 0.0; //Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
     }
 
     FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	// Weigh data
     FFT.compute(FFTDirection::Forward); // Compute FFT
     FFT.complexToMagnitude(); // Compute magnitudes 
+
     
-
-
-    /*
-    //Print FFT
-    for (uint16_t i = 0; i < LEN; i++)
-    {
-      if(i == 0)
-      {
-        Serial.print("[");
-      }
-      Serial.print(vReal[i],15);
-
-      if(i<(LEN-1))
-      {
-        if (i == (LEN/3) ||i == (2*LEN/3) || i == (LEN/2))
-        {
-          Serial.println(",");
-        }
-        else
-        {
-          Serial.print(", ");
-        }
-      }
-      else
-      {
-        Serial.print("]");
-      }
-      
-      Serial.print(((i * 1.0 * SAMPLE_RATE) / LEN));
-      Serial.print(" ");
-      Serial.println(vReal[i], 4);
-    }
-   
-    
-    for(unsigned int coeff = 0; coeff < 5; coeff++)
-    {
-      double mfcc_result = GetCoefficient(vReal, SAMPLE_RATE, 48, 64, coeff);
-      Serial.print(coeff);
-      Serial.print("  ");
-      Serial.println(mfcc_result);
-    }*/
-    
-
-
     
     double mfcc1 = GetCoefficient(vReal, SAMPLE_RATE, 48, 64, 0);
     double mfcc2 = GetCoefficient(vReal, SAMPLE_RATE, 48, 64, 1);
@@ -261,48 +253,67 @@ void loop()
     double mfcc5 = GetCoefficient(vReal, SAMPLE_RATE, 48, 64, 4);
     
     
+    //['Peak', 'RMS', 'CrestF', 'Kurtosis', 'ZeroCross', 'MFCC1', 'MFCC2', 'MFCC3', 'MFCC4', 'MFCC5', 'Class']
+    float entrada[10] = {maxAbs, rms, crestFactor, kurtosis, cruzamentosZero, mfcc1, mfcc2, mfcc3, mfcc4, mfcc5};
+    
+    if (!tf.predict(entrada).isOk())
+    {
+      Serial.println(tf.exception.toString());
+    }
+    
+    
+    float pred = tf.output(0);
+    Serial.println(pred);
+
+    if(pred > trust){
+      digitalWrite(LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED, LOW);
+    }
     
     // Exibir resultados
-    Serial.print(media, 6);
-    Serial.print(", ");
-    Serial.print(maior, 6);
-    Serial.print(", ");
-    Serial.print(maxAbs, 6);
-    Serial.print(", ");
-    Serial.print(diferencaMaiorMenor, 6);
-    Serial.print(", ");
-    Serial.print(rms, 6);
-    Serial.print(", ");
-    Serial.print(variancia, 6);
-    Serial.print(", ");
-    Serial.print(desvioPadrao, 6);
-    Serial.print(", ");
-    Serial.print(power, 6);
-    Serial.print(", ");
-    Serial.print(crestFactor, 6);
-    Serial.print(", ");
-    Serial.print(shapeFactor, 6);
-    Serial.print(", ");
-    Serial.print(impulseFactor, 6);
-    Serial.print(", ");
-    Serial.print(kurtosis, 6);
-    Serial.print(", ");
-    Serial.print(skewness, 6);
-    Serial.print(", ");
-    Serial.print(clearanceFactor, 6);
-    Serial.print(", ");
-    Serial.print(cruzamentosZero);
-    Serial.print(", ");
-    Serial.print(mfcc1, 6);
-    Serial.print(", ");
-    Serial.print(mfcc2, 6);
-    Serial.print(", ");
-    Serial.print(mfcc3, 6);
-    Serial.print(", ");
-    Serial.print(mfcc4, 6);
-    Serial.print(", ");
-    Serial.println(mfcc5, 6);
-
+    //Serial.print(media, 6);
+    //Serial.print(", ");
+    //Serial.print(maior, 6);
+    //Serial.print(", ");
+    //Serial.print(maxAbs, 6);
+    //Serial.print(", ");
+    //Serial.print(diferencaMaiorMenor, 6);
+    //Serial.print(", ");
+    //Serial.print(rms, 6);
+    //Serial.print(", ");
+    //Serial.print(variancia, 6);
+    //Serial.print(", ");
+    //Serial.print(desvioPadrao, 6);
+    //Serial.print(", ");
+    //Serial.print(power, 6);
+    //Serial.print(", ");
+    //Serial.print(crestFactor, 6);
+    //Serial.print(", ");
+    //Serial.print(shapeFactor, 6);
+    //Serial.print(", ");
+    //Serial.print(impulseFactor, 6);
+    //Serial.print(", ");
+    //Serial.print(kurtosis, 6);
+    //Serial.print(", ");
+    //Serial.print(skewness, 6);
+    //Serial.print(", ");
+    //Serial.print(clearanceFactor, 6);
+    //Serial.print(", ");
+    //Serial.print(cruzamentosZero);
+    //Serial.print(", ");
+    //Serial.print(mfcc1, 6);
+    //Serial.print(", ");
+    //Serial.print(mfcc2, 6);
+    //Serial.print(", ");
+    //Serial.print(mfcc3, 6);
+    //Serial.print(", ");
+    //Serial.print(mfcc4, 6);
+    //Serial.print(", ");
+    //Serial.println(mfcc5, 6);
+    
+    
   }
-  antbotao = botao;
 }
